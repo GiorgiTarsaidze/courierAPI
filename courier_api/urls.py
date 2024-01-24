abdffd770a73f0047d6d9f410987b0b366b03a7f
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-01-24 20:37:18 +0400

    Initial Project Setup

---
+++ courier_api/urls.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Courier Delivery Service API",
+        default_version="v1",
+        description="API for managing courier deliveries",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
