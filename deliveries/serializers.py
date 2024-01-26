6cc79c54080cd6a8a4791d24ce8970e84740ebbb
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-01-26 21:11:46 +0400

    fixed authentication bug

--- deliveries/serializers.py
+++ deliveries/serializers.py
@@ -5,6 +5,34 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'role', 'is_superuser', 'is_staff', 'password']
+        extra_kwargs = {
+            'password': {'write_only':True}
+        }
+
+    def create(self, validated_data):
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            role=validated_data['role'],
+            is_superuser=validated_data['is_superuser'],
+            is_staff=validated_data['is_staff']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.username = validated_data.get('username', instance.username)
+        instance.role = validated_data.get('role', instance.role)
+        instance.is_superuser = validated_data.get('is_superuser', instance.is_superuser)
+        instance.is_staff = validated_data.get('is_staff', instance.is_staff)
+
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
+
 
 class ParcelSerializer(serializers.ModelSerializer):
     class Meta:
--- deliveries/views.py
+++ deliveries/views.py
@@ -1,10 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import CustomUser, Parcel, DeliveryProof
 from .serializers import *
 
 class CustomUserViewset(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ParcelViewset(viewsets.ModelViewSet):
     queryset = Parcel.objects.all()
